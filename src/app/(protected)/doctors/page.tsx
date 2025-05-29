7ba200c3faf297860469ffc06767ad6c2f644a06
Lucasvalves <lucasvalves98@gmail.com>
2025-05-29 17:31:53 -0300

    feat: ensure user has clinic on doctors page

--- src/app/(protected)/doctors/page.tsx
+++ src/app/(protected)/doctors/page.tsx
@@ -1,10 +1,24 @@
 import { Plus } from "lucide-react";
+import { headers } from "next/headers";
+import { redirect } from "next/navigation";
 
 import { Button } from "@/components/ui/button";
 import { PageActions, PageContainer, PageContent, PageDescription, PageHeader, PageHeaderContent, PageTitle } from "@/components/ui/page-container";
+import { auth } from "@/lib/auth";
 
 
-const DoctorsPage = () => {
+const DoctorsPage = async () => {
+  const session = await auth.api.getSession({
+    headers: await headers()
+  })
+  if (!session?.user) {
+    redirect("/authentication")
+  }
+
+  if (!session?.user.clinic) {
+    redirect("/clinics-form")
+  }
+
   return (
     <PageContainer>
       <PageHeader>
@@ -21,9 +35,9 @@ const DoctorsPage = () => {
           </Button>
         </PageActions>
       </PageHeader>
-        <PageContent>
-          <h1>Médicos</h1>
-        </PageContent>
+      <PageContent>
+        <h1>Médicos</h1>
+      </PageContent>
     </PageContainer>
   )
 }
--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -28,14 +28,14 @@ export const auth = betterAuth({
         }
       })
       //TODO: Ao adaptar para o usuário ter mais clínicas, deve-se mudar esse código para retornar todas as clínicas do usuário.
-      const clinic = clinics[0]
+      const clinic = clinics?.[0]
       return {
         user: {
           ...user,
-          clinic: {
-            id: clinic.clinicId,
+          clinic: clinic?.clinicId ? {
+            id: clinic?.clinicId,
             name: clinic?.clinic?.name,
-          }
+          } : undefined,
         },
         ...session,
       }
