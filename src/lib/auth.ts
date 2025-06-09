378ddebc8cc23ec4a0bd26ed760785458325cc53
Lucasvalves <lucasvalves98@gmail.com>
2025-06-09 10:53:32 -0300

    feat: enhance dashboard and subscription management with new components and redirects

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -3,46 +3,73 @@ import { drizzleAdapter } from "better-auth/adapters/drizzle";
 import { customSession } from "better-auth/plugins";
 import { eq } from "drizzle-orm";
 
-import {db} from '@/db';
+import { db } from "@/db";
 import * as schema from "@/db/schema";
-import { usersToClinicsTable } from "@/db/schema";
+import { usersTable, usersToClinicsTable } from "@/db/schema";
 
 export const auth = betterAuth({
   database: drizzleAdapter(db, {
-    provider: "pg", 
+    provider: "pg",
     usePlural: true,
     schema,
-  }), 
+  }),
   socialProviders: {
-    google: { 
-      clientId: process.env.GOOGLE_CLIENT_ID as string, 
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string, 
-    }, 
-  },  
+    google: {
+      clientId: process.env.GOOGLE_CLIENT_ID as string,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+    },
+  },
   plugins: [
-    customSession(async({user, session}) => {
-      const clinics = await db.query.usersToClinicsTable.findMany({
-        where: eq(usersToClinicsTable.userId, user.id),
-        with: {
-          clinic: true,
-        }
-      })
-      //TODO: Ao adaptar para o usuário ter mais clínicas, deve-se mudar esse código para retornar todas as clínicas do usuário.
-      const clinic = clinics?.[0]
+    customSession(async ({ user, session }) => {
+      // TODO: colocar cache
+      const [userData, clinics] = await Promise.all([
+        db.query.usersTable.findFirst({
+          where: eq(usersTable.id, user.id),
+        }),
+        db.query.usersToClinicsTable.findMany({
+          where: eq(usersToClinicsTable.userId, user.id),
+          with: {
+            clinic: true,
+            user: true,
+          },
+        }),
+      ]);
+      // TODO: Ao adaptar para o usuário ter múltiplas clínicas, deve-se mudar esse código
+      const clinic = clinics?.[0];
       return {
         user: {
           ...user,
-          clinic: clinic?.clinicId ? {
-            id: clinic?.clinicId,
-            name: clinic?.clinic?.name,
-          } : undefined,
+          plan: userData?.plan,
+          clinic: clinic?.clinicId
+            ? {
+                id: clinic?.clinicId,
+                name: clinic?.clinic?.name,
+              }
+            : undefined,
         },
-        ...session,
-      }
+        session,
+      };
     }),
   ],
-  user: { 
+  user: {
     modelName: "usersTable",
+    additionalFields: {
+      stripeCustomerId: {
+        type: "string",
+        fieldName: "stripeCustomerId",
+        required: false,
+      },
+      stripeSubscriptionId: {
+        type: "string",
+        fieldName: "stripeSubscriptionId",
+        required: false,
+      },
+      plan: {
+        type: "string",
+        fieldName: "plan",
+        required: false,
+      },
+    },
   },
   session: {
     modelName: "sessionsTable",
@@ -56,5 +83,4 @@ export const auth = betterAuth({
   emailAndPassword: {
     enabled: true,
   },
-
 });
